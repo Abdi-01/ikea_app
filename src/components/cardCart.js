16bc81574e6599c8547022325f3d4078aa0bc24f
Abdi-01 <abdialghi@gmail.com>
2021-04-26 11:56:27 +0700

    ikea-app : checkout

--- src/components/cardCart.js
+++ src/components/cardCart.js
@@ -1,8 +1,38 @@
 import React from 'react';
 import { Body, Card, CardItem, Left, Right, Thumbnail } from 'native-base';
 import { Text, Button, Icon } from 'react-native-elements';
+import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
+import { URL_API } from '../helper'
+import { updateCart } from '../actions'
 
-const CardCart = ({ data }) => {
+const CardCart = ({ data, idx }) => {
+
+    const dispatch = useDispatch()
+
+    const { cart, iduser } = useSelector(({ userReducer }) => {
+        return {
+            cart: userReducer.cart,
+            iduser: userReducer.id
+        }
+    })
+
+    const onBtQty = (type) => {
+        if (type == "inc") {
+            cart[idx].qty += 1
+        } else if (type == "dec") {
+            cart[idx].qty -= 1
+        }
+        cart[idx].subTotal = cart[idx].qty * cart[idx].harga
+
+        axios.patch(URL_API + `/users/${iduser}`, { cart })
+            .then(res => {
+                dispatch(updateCart(res.data.cart))
+            }).catch(err => {
+                console.log(err)
+            })
+    }
+
     return (
         <Card>
             <CardItem>
@@ -16,11 +46,11 @@ const CardCart = ({ data }) => {
                 <Right style={{ flex: 1, flexDirection: 'row', justifyContent: 'center' }}>
                     <Button type="outline" icon={
                         <Icon type="feather" name="minus" size={15} />
-                    } />
-                    <Text h4 style={{marginHorizontal:10}}>{data.qty}</Text>
+                    } onPress={() => onBtQty("dec")} />
+                    <Text h4 style={{ marginHorizontal: 10 }}>{data.qty}</Text>
                     <Button type="outline" icon={
                         <Icon type="feather" name="plus" size={15} />
-                    } />
+                    } onPress={() => onBtQty("inc")} />
                 </Right>
             </CardItem>
         </Card>
--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,24 +1,55 @@
+import axios from 'axios';
 import React from 'react';
 import { View, FlatList } from 'react-native';
 import { Text, Button } from 'react-native-elements';
 import { heightPercentageToDP as hp, widthPercentageToDP as wp } from 'react-native-responsive-screen';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { updateCart } from '../actions';
 import CardCart from '../components/cardCart';
+import { URL_API } from '../helper';
 
 const CartPage = (props) => {
-    const { cart } = useSelector(({ userReducer }) => {
+    const dispatch = useDispatch()
+    const { cart, iduser, username } = useSelector(({ userReducer }) => {
         console.log("cek", userReducer.cart)
         return {
-            cart: userReducer.cart
+            cart: userReducer.cart,
+            iduser: userReducer.id,
+            username: userReducer.username
         }
     })
 
     const totalPayment = () => {
         let total = 0;
-        cart.forEach(item => {
-            total += item.subTotal
-        })
+        if (cart.length > 0) {
+            cart.forEach(item => {
+                total += item.subTotal
+            })
+        }
         return total
+    }
+
+    const onBtCheckout = () => {
+        // userTransaction = {iduser,username,tgl_transaksi,status,total_payment,detail_pembelian}
+        let date = new Date()
+        axios.post(URL_API + `/userTransaction`, {
+            iduser,
+            username,
+            tgl_transaksi: `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`,
+            total_payment: totalPayment(),
+            status: 'Unpaid',
+            detail_pembelian: cart
+        }).then(res => {
+            axios.patch(URL_API + `/users/${iduser}`, { cart: [] })
+                .then(resPatch => {
+                    dispatch(updateCart(resPatch.data.cart))
+                    props.navigation.navigate("Profile")
+                }).catch(errPatch => {
+                    console.log(errPatch)
+                })
+        }).catch(err => {
+            console.log(err)
+        })
     }
 
     return (
@@ -41,6 +72,7 @@ const CartPage = (props) => {
                     containerStyle={{ width: wp('40%') }}
                     buttonStyle={{ backgroundColor: '#FBD914' }}
                     titleStyle={{ color: '#0058AB' }}
+                    onPress={onBtCheckout}
                 />
             </View>
         </View>
